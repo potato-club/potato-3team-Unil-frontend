d3348e36e389289034a5afebb7c16b74acd73680
UNNEW-main <1970005826c@gmail.com>
2021-08-01 22:09:51 +0900

    header 및 mainPage 작업

--- src/Root.jsx
+++ src/Root.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import { Header } from './components/index';
 import { MainPage, ItemListPage } from './pages/index';
+
 const Root = () => {
   return (
     <BrowserRouter>
       <Header></Header>
-      <Route path="/" exact component={MainPage} />
-      <Route path="/itemList" component={ItemListPage} />
+      <div style={{ margin: '0 10%' }}>
+        <Route path="/" exact component={MainPage} />
+        <Route path="/itemList" component={ItemListPage} />
+      </div>
     </BrowserRouter>
   );
 };
--- src/pages/mainPage/MainPage.jsx
+++ src/pages/mainPage/MainPage.jsx
@@ -1,5 +1,15 @@
 import React from 'react';
-
+import cover from './mainPageCover.jpg';
+import styled from '@emotion/styled';
 export const MainPage = () => {
-  return <div>MainPage</div>;
+  return (
+    <div>
+      <MainPageCover src={cover}></MainPageCover>
+    </div>
+  );
 };
+
+const MainPageCover = styled.img`
+  width: 100%;
+  height: 100%;
+`;
