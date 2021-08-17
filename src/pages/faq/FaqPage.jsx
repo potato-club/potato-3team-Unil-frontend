7828abf8d40d6fab4fb3855374ce70a9e1a21b18
UNNEW-main <1970005826c@gmail.com>
2021-08-17 16:22:19 +0900

    AllReview 작업

--- src/pages/faq/FaqPage.jsx
+++ src/pages/faq/FaqPage.jsx
@@ -2,15 +2,19 @@ import React from 'react';
 import styled from '@emotion/styled';
 import { PageTitle } from 'components';
 import { AllReview, Notice, ContactUs } from './components/index';
-export const FaqPage = (props) => {
-  console.log(props);
 
+export const FaqPage = (props) => {
   return (
-    <div>
+    <Section>
       <PageTitle pageTitle={props.location.state.type}></PageTitle>
       {props.match.params.id === 'review' && <AllReview></AllReview>}
       {props.match.params.id === 'contactUs' && <ContactUs></ContactUs>}
       {props.match.params.id === 'notice' && <Notice></Notice>}
-    </div>
+    </Section>
   );
 };
+
+const Section = styled.div`
+  width: 90%;
+  margin: auto;
+`;
--- src/pages/faq/components/AllReview.jsx
+++ src/pages/faq/components/AllReview.jsx
@@ -1,5 +1,62 @@
 import React from 'react';
+import styled from '@emotion/styled';
+import { customColor } from 'constants/index';
 
 export const AllReview = () => {
-  return <div>review</div>;
+  return (
+    <Section>
+      <TagWrapper>
+        <GradeTag>평점</GradeTag>
+        <ProductTag>제품</ProductTag>
+        <TitleTag>제목</TitleTag>
+        <WriterTag>작성자</WriterTag>
+        <DateTag>작성날짜</DateTag>
+      </TagWrapper>
+      <div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </Section>
+  );
 };
+
+const Section = styled.div``;
+
+const TagWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  border-top: 2px solid ${customColor.fontMainColor};
+  border-bottom: 1px solid ${customColor.fontMainColor};
+  font-weight: bold;
+  padding: 7px 0;
+`;
+
+const GradeTag = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 15%;
+`;
+
+const ProductTag = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 15%;
+`;
+const TitleTag = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 40%;
+`;
+const WriterTag = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 15%;
+`;
+const DateTag = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 15%;
+`;
